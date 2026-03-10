ea6541ad32cf984aa16f4a9a204ba801c4198f33
Parzy2212 <slz7122004@gmail.com>
2026-03-10 12:34:52 +0700

    Fix sitemap route for static export with force-static

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 const BASE_URL = 'https://www.alan-coffee-travel.com'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
